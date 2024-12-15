0bbb081840b742d11cc983faaf40a47579e81928
Kunal Bhatia <74307421+kunalbhatia2601@users.noreply.github.com>
2024-12-16 03:46:40 +0530

    Solution to Print Even Numbers

--- Print Even Numbers@@675cac2c526f844d7db23a23/code.py
+++ Print Even Numbers@@675cac2c526f844d7db23a23/code.py
@@ -1,14 +1,7 @@
-# Python Playground
-numbers = [1, 2, 3, 4, 5]
+n = int(input())
 
-# Map numbers to their squares
-squares = [n ** 2 for n in numbers]
-print(f"Original numbers: {numbers}")
-print(f"Squared numbers: {squares}")
+for i in range(0, n):
+    x = int(input())
 
-# Filter for even numbers
-even_numbers = [n for n in numbers if n % 2 == 0]
-print(f"Even numbers: {even_numbers}")
-
-# Calculate sum
-numbers_sum = sum(numbers)
+    if(x%2==0):
+        print(x)
