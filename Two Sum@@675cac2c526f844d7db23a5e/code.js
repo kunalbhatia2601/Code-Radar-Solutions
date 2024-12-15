63456898d2cd351b19621e3b3b2a7b5e31eb6858
Kunal Bhatia <74307421+kunalbhatia2601@users.noreply.github.com>
2024-12-16 03:30:18 +0530

    Update code.js

--- Two Sum@@675cac2c526f844d7db23a5e/code.js
+++ Two Sum@@675cac2c526f844d7db23a5e/code.js
@@ -1,15 +1,2 @@
 // JavaScript Playground
 const numbers = [1, 2, 3, 4, 5];
-
-// Map numbers to their squares
-const squares = numbers.map(n => n * n);
-console.log('Original numbers:', numbers);
-console.log('Squared numbers:', squares);
-
-// Filter for even numbers
-const evenNumbers = numbers.filter(n => n % 2 === 0);
-console.log('Even numbers:', evenNumbers);
-
-// Calculate sum using reduce
-const sum = numbers.reduce((acc, curr) => acc + curr, 0);
-console.log('Sum of numbers:', sum);
