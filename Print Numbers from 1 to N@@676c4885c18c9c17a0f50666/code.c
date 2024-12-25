5ffad716fe5b043acf8e2fddc3f3ea153c64ea7f
Kunal Bhatia <74307421+kunalbhatia2601@users.noreply.github.com>
2024-12-26 00:41:15 +0530

    Solution to Print Numbers from 1 to N

--- Print Numbers from 1 to N@@676c4885c18c9c17a0f50666/code.c
+++ Print Numbers from 1 to N@@676c4885c18c9c17a0f50666/code.c
@@ -1,47 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    // Create an array
-    int numbers[] = {1, 2, 3, 4, 5};
-    int size = sizeof(numbers) / sizeof(numbers[0]);
+    
+    int n;
+    scanf("%d", &n);
 
-    // Print original numbers
-    printf("Original numbers: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", numbers[i]);
-    }
-        
-    printf("\n");
+    char c[100] = "";
 
-    // Calculate squares
-    int squares[size];
-    for (int i = 0; i < size; i++) {
-        squares[i] = numbers[i] * numbers[i];
-    }
+    for(int i=0; i<n; i++)
+        c = c + i+1 + " ";
 
-    printf("Squared numbers: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", squares[i]);
-    }
-
-    printf("\n");
-
-    // Filter even numbers
-    printf("Even numbers: ");
-    for (int i = 0; i < size; i++) {
-        if (numbers[i] % 2 == 0) {
-            printf("%d ", numbers[i]);
-        }
-    }
-
-    printf("\n");
-
-    // Calculate sum
-    int sum = 0;
-    for (int i = 0; i < size; i++) {
-        sum += numbers[i];
-    }
-    printf("Sum of numbers: %d\n", sum);
+    printf("%s", c);
 
     return 0;
 }
