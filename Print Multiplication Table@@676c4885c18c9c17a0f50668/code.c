76df03ef781642c4bb3106e6393fc012d039ddd0
Kunal Bhatia <74307421+kunalbhatia2601@users.noreply.github.com>
2025-01-07 17:01:41 +0530

    Solution to Print Multiplication Table

--- Print Multiplication Table@@676c4885c18c9c17a0f50668/code.c
+++ Print Multiplication Table@@676c4885c18c9c17a0f50668/code.c
@@ -10,3 +10,111 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+int main() {
+    
+    int n;
+    scanf("%d", &n);
+
+    int sum = 0;
+    for (int i = 1; i <= 10; i++)
+        printf("%d x %d = %d\n", n, i, n*i);
+
+    return 0;
+}
+#include <stdio.h>
+int main() {
+    
+    int n;
+    scanf("%d", &n);
+
+    int sum = 0;
+    for (int i = 1; i <= 10; i++)
+        printf("%d x %d = %d\n", n, i, n*i);
+
+    return 0;
+}
+#include <stdio.h>
+int main() {
+    
+    int n;
+    scanf("%d", &n);
+
+    int sum = 0;
+    for (int i = 1; i <= 10; i++)
+        printf("%d x %d = %d\n", n, i, n*i);
+
+    return 0;
+}
+#include <stdio.h>
+int main() {
+    
+    int n;
+    scanf("%d", &n);
+
+    int sum = 0;
+    for (int i = 1; i <= 10; i++)
+        printf("%d x %d = %d\n", n, i, n*i);
+
+    return 0;
+}
+#include <stdio.h>
+int main() {
+    
+    int n;
+    scanf("%d", &n);
+
+    int sum = 0;
+    for (int i = 1; i <= 10; i++)
+        printf("%d x %d = %d\n", n, i, n*i);
+
+    return 0;
+}
+#include <stdio.h>
+int main() {
+    
+    int n;
+    scanf("%d", &n);
+
+    int sum = 0;
+    for (int i = 1; i <= 10; i++)
+        printf("%d x %d = %d\n", n, i, n*i);
+
+    return 0;
+}
+#include <stdio.h>
+int main() {
+    
+    int n;
+    scanf("%d", &n);
+
+    int sum = 0;
+    for (int i = 1; i <= 10; i++)
+        printf("%d x %d = %d\n", n, i, n*i);
+
+    return 0;
+}
+#include <stdio.h>
+int main() {
+    
+    int n;
+    scanf("%d", &n);
+
+    int sum = 0;
+    for (int i = 1; i <= 10; i++)
+        printf("%d x %d = %d\n", n, i, n*i);
+
+    return 0;
+}
+#include <stdio.h>
+int main() {
+    
+    int n;
+    scanf("%d", &n);
+
+    int sum = 0;
+    for (int i = 1; i <= 10; i++)
+        printf("%d x %d = %d\n", n, i, n*i);
+
+    return 0;
+}
