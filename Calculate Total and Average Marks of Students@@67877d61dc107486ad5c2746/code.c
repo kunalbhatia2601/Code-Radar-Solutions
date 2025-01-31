daa07730790223f7856c267d61ec015cc73589ed
Kunal Bhatia <74307421+kunalbhatia2601@users.noreply.github.com>
2025-01-31 18:54:50 +0530

    Solution to Calculate Total and Average Marks of Students

---
+++ Calculate Total and Average Marks of Students@@67877d61dc107486ad5c2746/code.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    printf("%s", welcome());
+    return 0;
+}#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    printf("%s", welcome());
+    return 0;
+}#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    printf("%s", welcome());
+    return 0;
+}#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    printf("%s", welcome());
+    return 0;
+}#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    printf("%s", welcome());
+    return 0;
+}#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    printf("%s", welcome());
+    return 0;
+}#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    printf("%s", welcome());
+    return 0;
+}#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    printf("%s", welcome());
+    return 0;
+}#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    printf("%s", welcome());
+    return 0;
+}
