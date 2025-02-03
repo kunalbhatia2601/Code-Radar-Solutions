09c86b026801823e04feca14e7eea40f6783a4cc
Kunal Bhatia <74307421+kunalbhatia2601@users.noreply.github.com>
2025-02-04 00:43:30 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,47 +1,30 @@
 #include <stdio.h>
-
 int main() {
-    // Create an array
-    int numbers[] = {1, 2, 3, 4, 5};
-    int size = sizeof(numbers) / sizeof(numbers[0]);
-
-    // Print original numbers
-    printf("Original numbers: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", numbers[i]);
-    }
-        
-    printf("\n");
-
-    // Calculate squares
-    int squares[size];
-    for (int i = 0; i < size; i++) {
-        squares[i] = numbers[i] * numbers[i];
+    int n, k;
+    scanf("%d", &n);
+    if (n <= 0) {
+        return 1;
     }
 
-    printf("Squared numbers: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", squares[i]);
+    int nums[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
     }
 
-    printf("\n");
+    scanf("%d", &k);
+    k = k % n;
 
-    // Filter even numbers
-    printf("Even numbers: ");
-    for (int i = 0; i < size; i++) {
-        if (numbers[i] % 2 == 0) {
-            printf("%d ", numbers[i]);
+    for (int i = 0; i < k; i++) {
+        int temp = nums[n - 1];
+        for (int j = n - 1; j > 0; j--) {
+            nums[j] = nums[j - 1];
         }
+        nums[0] = temp;
     }
 
-    printf("\n");
-
-    // Calculate sum
-    int sum = 0;
-    for (int i = 0; i < size; i++) {
-        sum += numbers[i];
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", nums[i]);
     }
-    printf("Sum of numbers: %d\n", sum);
 
     return 0;
 }
