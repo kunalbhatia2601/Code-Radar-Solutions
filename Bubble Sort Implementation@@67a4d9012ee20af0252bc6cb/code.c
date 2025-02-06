a7452cd6cbad79cc1c4680a5434de94af68d460a
Kunal Bhatia <74307421+kunalbhatia2601@users.noreply.github.com>
2025-02-07 00:58:07 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
-char* welcome() {
-    return "Welcome to Code Radar!";
-}
-
-int main() {
-    printf("%s", welcome());
-    return 0;
+void bubblesort(int ary[], int n)
+{
+    printf("%d", n);
 }
