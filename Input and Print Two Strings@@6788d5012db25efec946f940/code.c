51146a9e68a6d45edfb2cc4c2352eb15ece3c277
Kunal Bhatia <74307421+kunalbhatia2601@users.noreply.github.com>
2025-03-19 18:40:00 +0530

    Solution to Input and Print Two Strings

--- Input and Print Two Strings@@6788d5012db25efec946f940/code.c
+++ Input and Print Two Strings@@6788d5012db25efec946f940/code.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
-int main(){
-    int n;
-    scanf("%d",&n);
-    for(int i = 1;i<=500;i++){
-        printf("%d %c",i,i);
-    }
-    return 0;
+
+int main() {
+ FILE *filePointer;
+
+ char filePath[] = "./a.out";
+
+ filePointer = fopen(filePath, "r");
+
+ if (filePointer == NULL) {
+ printf("Could not open file %s\n", filePath);
+ return 1; 
+ }
+
+ char line[100];
+ while (fgets(line, sizeof(line), filePointer)) {
+ printf("%s", line);
+ }
+
+ fclose(filePointer);
+
+ return 0;
 }
