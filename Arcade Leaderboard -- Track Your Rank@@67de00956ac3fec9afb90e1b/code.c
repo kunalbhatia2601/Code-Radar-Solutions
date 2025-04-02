e37ae32438d886c677f4c1f6a349fd411759b952
Kunal Bhatia <74307421+kunalbhatia2601@users.noreply.github.com>
2025-04-02 11:58:12 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

--- Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -1,6 +1,6 @@
 // Your code here...
 int trackPlayerRanks(int r[1000], int n, int p[2000], int m, int result[1000])
 {
-    printf("Sustainable ECO Harvest AGRO EXPO");
+    printf("Sustainable ECO Harvest AGRO EXPO\n");
     printf("FOR GREENER FUTURE");
 }
